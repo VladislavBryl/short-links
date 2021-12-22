ab94b64e04bab582c15fb2650b8104d98f07c67e
VladislavBryl <54996000+VladislavBryl@users.noreply.github.com>
2021-12-22 15:50:18 +0200

    update -12

--- client/src/serviceWorker.js
+++ client/src/serviceWorker.js
@@ -13,9 +13,7 @@ export function register(config) {
         // The URL constructor is available in all browsers that support SW.
         const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
         if (publicUrl.origin !== window.location.origin) {
-            // Our service worker won't work if PUBLIC_URL is on a different origin
-            // from what our page is served on. This might happen if a CDN is used to
-            // serve assets; see https://github.com/facebook/create-react-app/issues/2374
+
             return;
         }
 
@@ -54,9 +52,7 @@ function registerValidSW(swUrl, config) {
                 installingWorker.onstatechange = () => {
                     if (installingWorker.state === 'installed') {
                         if (navigator.serviceWorker.controller) {
-                            // At this point, the updated precached content has been fetched,
-                            // but the previous service worker will still serve the older
-                            // content until all client tabs are closed.
+
                             console.log(
                                 'New content is available and will be used when all ' +
                                 'tabs for this page are closed. See https://bit.ly/CRA-PWA.'
